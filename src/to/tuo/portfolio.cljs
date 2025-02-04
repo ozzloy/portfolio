a56aef72384714311431636a4d4f5415c74488fe
daniel watson <ozzloy@each.do>
2025-02-03 20:26:05 -0800

    switch back to react 18

--- src/to/tuo/portfolio.cljs
+++ src/to/tuo/portfolio.cljs
@@ -1,13 +1,17 @@
 (ns to.tuo.portfolio
-  (:require [reagent.dom :as rdom]))
+  (:require
+   ["react-dom/client" :refer [createRoot]]
+   [reagent.core :as r]))
 
 (defn hello []
   [:div
    [:h1 "hello from reagent"]
    [:p "this is a paragraph"]])
 
+(defonce root (createRoot (js/document.getElementById "app")))
+
 (defn ^:export init []
-  (js/console.log "hello, figwheel!")
-  (rdom/render [hello] (js/document.getElementById "app")))
+  (js/console.log "hello, figwheel!!")
+  (.render root (r/as-element [hello])))
 
 (init)
