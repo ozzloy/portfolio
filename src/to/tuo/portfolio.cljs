023460fcbc0dece54e9b2e96c6b6403167eea851
daniel watson <ozzloy@each.do>
2025-02-03 18:15:27 -0800

    do reagent hello world

--- src/to/tuo/portfolio.cljs
+++ src/to/tuo/portfolio.cljs
@@ -1,6 +1,13 @@
-(ns to.tuo.portfolio)
+(ns to.tuo.portfolio
+  (:require [reagent.dom :as rdom]))
 
-(defn init []
-  (js/console.log "hello, figwheel"))
+(defn hello []
+  [:div
+   [:h1 "hello from reagent"]
+   [:p "this is a paragraph"]])
+
+(defn ^:export init []
+  (js/console.log "hello, figwheel!")
+  (rdom/render [hello] (js/document.getElementById "app")))
 
 (init)
